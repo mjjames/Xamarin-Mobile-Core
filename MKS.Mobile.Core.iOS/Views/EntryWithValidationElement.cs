591abc06f5c87a4d538398dd6f249db92e545411
Michael James <michael.james@mksoftwaresolutions.co.uk>
2014-04-15 14:48:50 -0100

    added new core monotouch.dialog entry elements the two provided can handle validation and numeric entry with a +/- button

---
+++ MKS.Mobile.Core.iOS/Views/EntryWithValidationElement.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using System.Drawing;
+
+namespace MKS.Mobile.Core.iOS.Views
+{
+    public class EntryWithValidationElement : EntryElement
+    {
+        public EntryWithValidationElement(string caption, string placeholder, string value)
+            : base(caption, placeholder, value)
+        {
+        }
+
+        public Func<UITextField, bool> Validate { get; set; }
+
+        protected override UITextField CreateTextField(RectangleF frame)
+        {
+            var textField = base.CreateTextField(frame);
+            if (Validate != null)
+            {
+                SetupValidation(textField);
+            }
+
+            return textField;
+        }
+
+        private void SetupValidation(UITextField textField)
+        {
+            var accessoryView = new UILabel
+            {
+                Text = "\u2B1C "
+            };
+            accessoryView.SizeToFit();
+            textField.RightViewMode = UITextFieldViewMode.Always;
+            textField.RightView = accessoryView;
+            textField.Ended += (o, e) =>
+            {
+                if (Validate == null)
+                {
+                    return;
+                }
+                accessoryView.Text = Validate(textField) ? "\u2705  " : "\u2B1C  ";
+            };
+        }
+
+    }
+}
---
+++ MKS.Mobile.Core.iOS/Views/NumericEntryElement.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using System.Drawing;
+
+namespace MKS.Mobile.Core.iOS.Views
+{
+    public class NumericEntryElement : EntryWithValidationElement
+    {
+
+        public NumericEntryElement(string caption, string placeholder, string value)
+            : base(caption, placeholder, value)
+        {
+            base.KeyboardType = UIKeyboardType.DecimalPad;
+        }
+
+        protected override UITextField CreateTextField(RectangleF frame)
+        {
+            var textField = base.CreateTextField(frame);
+
+            var toggleNumberType = new UIBarButtonItem("+/-", UIBarButtonItemStyle.Bordered, (sender, args) => NegateValue());
+
+            var toolbar = new UIToolbar(new RectangleF(0, 0, frame.Width, 30f))
+            {
+                Translucent = true
+            };
+
+
+            var items = new List<UIBarButtonItem>();
+            items.Add(toggleNumberType);
+            items.Add(new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace));
+
+            if (ReturnKeyType.HasValue && ReturnKeyType.Value == UIReturnKeyType.Done)
+            {
+
+                var done = new UIBarButtonItem(UIBarButtonSystemItem.Done);
+
+                done.Clicked += (o, e) =>
+                {
+                    if (ReturnKeyType.HasValue && ReturnKeyType.Value == UIReturnKeyType.Done)
+                    {
+                        ResignFirstResponder(true);
+                    }
+                    else
+                    {
+                        Parent.GetContainerTableView().NextResponder.BecomeFirstResponder();
+                    }
+                };
+                items.Add(done);
+            }
+
+            toolbar.SetItems(items.ToArray(), false);
+
+            textField.InputAccessoryView = toolbar;
+            return textField;
+        }
+
+        private void NegateValue()
+        {
+            float value;
+            if (float.TryParse(Value, out value))
+            {
+                Value = (-value).ToString();
+            }
+        }
+
+        public new UIKeyboardType KeyboardType
+        {
+            get
+            {
+                return UIKeyboardType.DecimalPad;
+            }
+        }
+
+    }
+}
