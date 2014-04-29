baa51e21c0022c9dee97f38fe8a6aafe4682e620
Michael James <michael.james@mksoftwaresolutions.co.uk>
2014-04-29 10:07:03 -0100

    added exceptionitemprocessor and the versioned base uses the exception library

---
+++ MKS.Mobile.Core/QueueProcessor/ExceptionItemProcessor.cs
@@ -0,0 +1,37 @@
+﻿using MK.ErrorTracking.DataObjects;
+using MKS.ErrorTracking.ServiceClient;
+using MKS.Mobile.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKS.Mobile.Core.QueueProcessor
+{
+    public class ExceptionItemProcessor : VersionedProcessorBase, IQueueItemProcessor
+    {
+        private ServiceDetails _serviceDetails;
+        private HttpMessageHandler _messageHandler;
+
+        public ExceptionItemProcessor(ServiceDetails serviceDetails, HttpMessageHandler messageHandler = null)
+            : base(typeof(ExceptionItem).AssemblyQualifiedName)
+        {
+            _serviceDetails = serviceDetails;
+            _messageHandler = messageHandler;
+        }
+
+        public async Task<bool> ProcessItem(object Item)
+        {
+            var exceptionItem = Item as ExceptionItem;
+            if (exceptionItem == null)
+            {
+                return false;
+            }
+            var result = await new ExceptionLogger(_serviceDetails, _messageHandler)
+                            .LogException(exceptionItem);
+            return result.IsSuccess;
+        }
+    }
+}
---
+++ MKS.Mobile.Core/QueueProcessor/VersionedItemBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKS.Mobile.Core.QueueProcessor
+{
+    public abstract class VersionedProcessorBase
+    {
+        protected string ItemType;
+        protected string ItemTypeAssembly;
+        protected string ItemTypeMajorVersion;
+
+        protected VersionedProcessorBase(string assemblyQualifiedName)
+        {
+            var parts = GetParts(assemblyQualifiedName);
+            ItemType = parts[0];
+            ItemTypeAssembly = parts[1];
+            ItemTypeMajorVersion = parts[2];
+        }
+
+
+        protected string[] GetParts(string assemblyQualifiedName)
+        {
+            var parts = assemblyQualifiedName.Split(',');
+            return new[]{
+                parts[0],
+                parts[1],
+                parts[2].Split('.')[0].Replace("Version=", "")
+            };
+        }
+
+        public bool CanProcessItem(string itemType)
+        {
+            //rather than check the entire assembly qualified name we only want to check the type name, assembly name and the Major version of the assembly
+            //we want to handle updates to the assembly and in theory as long as the major version doesnt change we should be able to parse it
+            var parts = GetParts(itemType);
+            return parts[0] == ItemType
+                    && parts[1] == ItemTypeAssembly
+                    && parts[2] == ItemTypeMajorVersion;
+        }
+
+    }
+}
